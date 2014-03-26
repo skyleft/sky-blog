2cc73fd5bd40f99c7b3f9fe84bffc4ea79640af3
Andy Cheung <skyleft123@gmail.com>
2014-03-26 13:29:33 +0800

    add account module---1st step

---
+++ account/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+urlpatterns = patterns('account.views',
+    url('^register$','reg'),
+
+) 
--- skyblog/urls.py
+++ skyblog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.views.static import serve
+from account import urls as accountUrls
 import settings
 admin.autodiscover()
 
@@ -16,6 +17,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^account/',include(accountUrls)),
     url(r'^$','blog.views.index'),
     url(r'^post/(?P<post_id>\d+)/$','blog.views.post'),
     url(r'^category/(?P<category_id_tmp>\d+)/$','blog.views.category'),
